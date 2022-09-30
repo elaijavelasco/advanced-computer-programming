764bdc5e49c26a3821aa09642430ac545a210b18
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-09-30 21:46:34 +0800

    Create matrix-calc.py

---
+++ matrix-calc.py
@@ -0,0 +1,14 @@
+class Matrix:
+    
+    def __init__(self, matrix): 
+        self.matrix = matrix 
+
+    """
+    __init__ is a constructor for a class
+    
+    self is a parameter that refers to the instance of the object
+    
+    matrix is the parameter that will be stored in variables and it would be discarded if init method goes out of bound
+    
+    self.matrix sets matrix as member of the Matrix object
+    """
