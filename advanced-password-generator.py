0d3e86726abeea026ec7d4d82cf0b48280311cb0
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-10-02 14:59:08 +0800

    Update advanced-password-generator.py

--- advanced-password-generator.py
+++ advanced-password-generator.py
@@ -17,16 +17,40 @@ def generate_password(password_len):
 
         password = replace_w_number(password)
         password = replace_w_uppercase(password)
-        passwords.append(password)
+        passwords_list.append(password)
     
-    return passwords
+    return passwords_list
 
 def replace_w_number(new_password):
     for i in range(random.randrange(1,3)):
-        replace_idx = random.randrange(len(new_passsword)//2)
-        new_password = new_password[0:replace_idx] + str(random.randrage(10)) + new_password[replace_idx + 1:]
+        replace_idx = random.randrange(len(new_password)//2)
+        new_password = new_password[0:replace_idx] + str(random.randrange(10)) + new_password[replace_idx + 1:]
         return new_password 
 
+def replace_w_uppercase(new_password):
+    for i in range(random.randrange(1,3)):
+        replace_idx = random.randrange(len(new_password)//2)
+        new_password = new_password[0:replace_idx] + new_password[replace_idx].upper() + new_password[replace_idx + 1:]
+        return new_password
 
+def main():
+    
+    password_num = int (input("How many passwords you wan to generate? "))
+    print("Generating " +str(password_num) + " passwords")
 
+    pword_length = []
+    print("The minimum length of password should be 6")
+
+    for i in range(password_num):
+        length = int(input("Enter you desired password length on password [" + str(i+1) + "] : " ))
+        if length <= 6:
+            length = 6
+        pword_length.append(length)
+    
+    Password = generate_password(pword_length)
+
+    for i in range(password_num):
+        print ("Password [" +str(i+1) + "]" + " = " + Password[i]) 
+
+main()
 
