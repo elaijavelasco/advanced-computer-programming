f4cec00322bfc97138bbb9d600d30831124d1b20
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-10-04 21:45:48 +0800

    Create chapter-2.py

---
+++ chapter-2.py
@@ -0,0 +1,32 @@
+"""
+number = eval(input("Enter a number: "))
+print (number)
+
+print()
+answer = eval(input("Enter an equation: "))
+print (answer)
+
+convertToFloat = float(input("Enter a number: "))
+print (convertToFloat)
+
+x = 19
+print (float (x))
+
+x, y, z = 1,2,3
+print (x)
+print (y)
+print (z)
+print (x+y)
+print (y+z)
+print (z+x)
+
+myName = "Elaija"
+herName = "Santy"
+
+theName = myName
+myName = herName
+herName = theName
+
+print(myName)
+print (herName)
+"""
