0cfa2f5ba55260165364c8b9655f22443a17cb62
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-10-27 19:54:38 +0800

    Update purchasing-system.py

--- purchasing-system.py
+++ purchasing-system.py
@@ -1,3 +1,7 @@
+product_list = [{"id" : 21021, "name" : "Mouse", "quantity" : 100, "type" : "Gadget Accessory", "price" : 399}
+    {"id" : 21022, "name" : "Mouse Pad", "quantity" : 100, "type" : "Gadget Accessory", "price" : 199}
+    {"id" : 21023, "name" : "Keyboard", "quantity" : 50, "type" : "Gadget Accessory", "price" : 599}]
+
 class Main:
     def show_menu(self):
         while True:
