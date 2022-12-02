cc3de49d2b9876bbbf968c39fdfe2ba79fa0afb8
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-12-03 00:36:07 +0800

    Update text_file_app.py

--- Text File App/text_file_app.py
+++ Text File App/text_file_app.py
@@ -5,14 +5,14 @@ def app_menu():
     print("\n\nText File Creator")
     print("\nMain Menu:")
     print("1. Create a text file")
-    print("2. Edit text file")
+    print("2. Write on text file")
     print("3. Open text file")
     print("4. Delete text file")
     print("5. Exit")
 
     choice = ""
     try: 
-        choice = int(input("Enter chooice: "))
+        choice = int(input("\nEnter chooice: "))
     except ValueError:
         print("Invalid Input!")
     
@@ -49,4 +49,24 @@ def create_txt_file():
     input("Press any key to continue...")
     app_menu()
 
+def edit_txt_file():
+    file_name = input("Enter the txt file you want to edit: ")
+    while not os.path.isfile(file_name):
+        print("File not found...")
+        break
+    
+    print(file_name + " has been opened!")
+    file_content = input ("Write something on your text file: ")
 
+    while (file_content == ""):
+        print("Don't leave it blank...")
+        file_content = input ("Please write something on your text file: ")
+    
+    txt_file = open(file_name, "w")
+    txt_file.writelines(file_content)
+    txt_file.close()
+
+    print("\nEdited text file has been saved!")
+    input("Press any key to continue...")
+    app_menu()
+    
