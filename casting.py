3466623ce9197c3fca1d0d2584c419dc69563b92
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-12-02 22:54:52 +0800

    Delete casting.py

--- casting.py
+++
@@ -1,9 +0,0 @@
-#This is Python casting
-
-pi_value1 = int (3.141592653589793238)
-pi_value2 = float(3.141592653589793238)
-pi_value3 = str(3.141592653589793238)
-
-print(pi_value1)
-print(pi_value2)
-print(pi_value3)
